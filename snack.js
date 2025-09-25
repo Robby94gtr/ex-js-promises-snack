30dac685e8abd251aa0d1e80b9866e65eb17a2e2
Robby94gtr <robertogramazio94@gmail.com>
2025-09-25 11:20:10 +0200

    Correzione

--- snack.js
+++ snack.js
@@ -54,7 +54,27 @@ getPost(2)
     .catch(error => console.error(error));
  */
 
+//CORREZIONE :
 
+/* function getPost(id){
+    return new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(response => response.json())
+            .then(post => {
+                return fetch(`https://dummyjson.com/users/${post.userId}`)
+                    .then(response => response.json())
+                    .then(user => resolve({...post, user}))
+                    .catch(reject)
+            })
+            .catch(reject);
+    });
+} 
+
+
+
+getPost(2)
+    .then(post => console.log(post))
+    .catch(error => console.error(error)); */
 
 /* 🏆 Snack 2
 Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, genera un numero casuale tra 1 e 6. Tuttavia, nel 20% dei casi, il dado si "incastra" e la Promise va in reject. */
@@ -62,6 +82,7 @@ Crea la funzione lanciaDado() che restituisce una Promise che, dopo 3 secondi, g
 
 /* const lanciaDado = () => {
     return new Promise((resolve, reject) =>{
+        console.log('Sto lanciando il dado...')
         setTimeout(() => {
             const numero = Math.floor(Math.random() * 6) + 1;
             const incastrato = Math.random()
@@ -89,7 +110,7 @@ lanciaDado()
 /* 🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
 Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato. Se il numero esce due volte di fila, stampa "Incredibile!". */
 
-const creaLanciaDado = () => {
+/* const creaLanciaDado = () => {
     let ultimo;
     
     return () => {
@@ -121,4 +142,8 @@ lanciaDado()
 
 lanciaDado()
      .then(risultato => console.log("hai lanciato:", risultato))
-     .catch(error => console.log(error));
+     .catch(error => console.log(error)); */
+
+
+
+
